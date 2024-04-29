788f31be84ffde226f79b6baffb8e1c44c8f811f
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-29 17:48:45 -0400

    change the libc casting method

--- replace-sysroot/replace/libc_replace.c
+++ replace-sysroot/replace/libc_replace.c
@@ -9,21 +9,6 @@ unsigned long strlen(const char *str) {
     return (int)(s - str);
 }
 
-// Since the WebAssembly Interface (WAI) seems only support i32, i64, f32, f64
-// according to https://medium.com/wasmer/introducing-webassembly-interfaces-bb3c05bc671
-int convertBufToInt(const void *buf) {
-    const unsigned char *byteBuf = (const unsigned char *)buf;
-    int result = 0;
-
-    // Convert each byte into an int and shift them into right positions
-    result |= byteBuf[0] << 24;
-    result |= byteBuf[1] << 16;
-    result |= byteBuf[2] << 8;
-    result |= byteBuf[3];
-
-    return result;
-}
-
 typedef int                     int32_t;
 
 int32_t __imported_wasi_snapshot_preview1_lind_write(int32_t arg0, int32_t arg1) __attribute__((
@@ -36,7 +21,6 @@ void __lind_write(int32_t buf, int32_t nbyte) {
 }
 
 void write(const void *buf, int nbyte) {
-    float bufFloat = convertBufToInt(buf);
-    __lind_write((int32_t) bufFloat, (int32_t) nbyte);
+    __lind_write((int32_t) buf, (int32_t) nbyte);
 }
 
