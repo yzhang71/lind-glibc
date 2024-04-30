a227e8071b6b090ce43f752ae88e642a2cfd1b6f
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-30 14:37:36 -0400

    new line

--- replace-sysroot/test/hello.c
+++ replace-sysroot/test/hello.c
@@ -1,9 +1,9 @@
 #include <libc.h>
 
 int main() {
-    char str[] = "Hello from Coulson's WASM!";
+    char str[] = "Hello from Coulson's WASM!\n";
     char *buf = str;
-    write(buf, 27);
+    write(buf, 28);
 
     return 0;
 }
